27169b5db3b8896b83dad2b1f07767fcd1b77bc6
hendisantika <hendisantika@yahoo.co.id>
2024-01-05 08:53:42 +0700

    Create starter project 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/com/hendisantika/springreactiveperformance/SpringReactivePerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springreactiveperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactivePerformanceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringReactivePerformanceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springreactiveperformance/SpringReactivePerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springreactiveperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactivePerformanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
