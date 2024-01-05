046fcddf06b3b9f877e3c5002771ad033f1ad9cd
hendisantika <hendisantika@yahoo.co.id>
2024-01-05 09:00:14 +0700

    Add ProductService 🫸🌀✏️📗 :octocat:

---
+++ src/main/java/com/hendisantika/springreactiveperformance/virtualthreads/ProductService.java
@@ -0,0 +1,23 @@
+package com.hendisantika.springreactiveperformance.virtualthreads;
+
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-reactive-performance
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 1/5/24
+ * Time: 08:59
+ * To change this template use File | Settings | File Templates.
+ */
+@RequiredArgsConstructor
+public class ProductService {
+    private final String PRODUCT_ADDED_TO_CART_TOPIC = "product-added-to-cart";
+
+    private final ProductRepository repository;
+    private final DiscountService discountService;
+    private final KafkaTemplate<String, ProductAddedToCartEvent> kafkaTemplate;
+
+}
